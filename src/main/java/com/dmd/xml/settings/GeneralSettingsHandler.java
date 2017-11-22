da130d0eabe66d31bb33efd98cf8ea393ef1d790
vshtrash <vshtrash@inbox.lv>
2017-11-22 19:13:11 +0200

    xml handlers refactor

--- src/main/java/com/dmd/xml/settings/GeneralSettingsHandler.java
+++ src/main/java/com/dmd/xml/settings/GeneralSettingsHandler.java
@@ -2,18 +2,12 @@ package com.dmd.xml.settings;
 
 import com.dmd.model.settings.GeneralSettings;
 import com.dmd.xml.XmlConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.annotation.Resource;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 @Service
 public class GeneralSettingsHandler {
@@ -23,19 +17,22 @@ public class GeneralSettingsHandler {
 
 
     public GeneralSettings loadSettings(String fileName) throws IOException, JAXBException {
-        if (fileName == null || fileName == "") {
-            fileName = SETTING_FILE_NAME;
-        }
+        fileName = composeFileName(fileName);
         GeneralSettings generalSettings = (GeneralSettings) xmlConverter.convertFromXMLToObject(fileName);
         return generalSettings;
     }
 
     public void saveSettings(String fileName) throws IOException, JAXBException {
-        if (fileName == null || fileName == "") {
-            fileName = SETTING_FILE_NAME;
-        }
+        fileName = composeFileName(fileName);
         GeneralSettings generalSettings = new GeneralSettings(fileName);
         xmlConverter.convertFromObjectToXML(generalSettings, fileName);
     }
 
+
+    private String composeFileName(String fileName) {
+        if (fileName == null || fileName == "") {
+            fileName = SETTING_FILE_NAME;
+        }
+        return fileName;
+    }
 }
